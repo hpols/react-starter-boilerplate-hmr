5c16882184eac4ee70651f9c1b1c98bd94eca5e8
Hilde Pols <atschpe@gmail.com>
2022-07-08 15:17:13 +0200

    Update App.js
    "Switch" has been replaced by "Routes" in React-Router-DOM.
    
    Possibly also change this in your create tutorial (crash on first script-run)

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import importedComponent from 'react-imported-component';
 
 import Home from './Home';
@@ -22,11 +22,11 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Switch>
+        <Routes>
           <Route exact path="/" component={Home} />
           <Route exact path="/dynamic" component={AsyncDynamicPAge} />
           <Route component={AsyncNoMatch} />
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
